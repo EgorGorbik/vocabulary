0ffe7e70f49ab8cccd92e4c0358334760ac62ab6
EgorGorbik <Gorbik1966@mail.ru>
2020-01-13 18:51:38 +0300

    Updates

---
+++ precache-manifest.8b3ac93367191235f797b30aece4a3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e77cffe668f330e199d646ab9171f6",
+    "url": "/vocabulary/index.html"
+  },
+  {
+    "revision": "cd96dfeefdcd9500d64a",
+    "url": "/vocabulary/static/css/main.7c47578d.chunk.css"
+  },
+  {
+    "revision": "bd0cfc68598a1f5c08c7",
+    "url": "/vocabulary/static/js/2.cd2769fc.chunk.js"
+  },
+  {
+    "revision": "4903237e8076d4264eefedc98e8e8447",
+    "url": "/vocabulary/static/js/2.cd2769fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd96dfeefdcd9500d64a",
+    "url": "/vocabulary/static/js/main.9424357c.chunk.js"
+  },
+  {
+    "revision": "3248b7dc14959257b751",
+    "url": "/vocabulary/static/js/runtime-main.9ab706b6.js"
+  }
+]);
